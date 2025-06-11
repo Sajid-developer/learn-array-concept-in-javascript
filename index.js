58d8776b8650dbd105f0d9bb38b6d8ec38f3fc84
Sajid-developer <msasajid01.pro@gmail.com>
2025-06-11 23:34:24 +0530

    file modified.

--- index.js
+++ index.js
@@ -2,34 +2,35 @@ console.log("App running successfully...");
 
 // array concept with javascript ✅
 
-function arrProb(){
-        const nums = [23,3,3,7,8,7,7,3,7,14,7,7,65,43,12,97,67,76,74,65,45,70,32,39,63];
-        console.log(`Length of array is : ${nums.length}`);
-        let maxNum = nums[0];
-        let randNum = Math.floor(Math.random()*nums.length);
-        console.log(randNum);
-        let mRepNum=nums[randNum];
-        let sum=0,count=0;
+const nums = [23, 3, 3, 7, 8, 7, 3, , 14, 7, 65, 43, 12, 97, 67, 76, 74, 65, 45, 70, 32, 39, 63];
 
-         for(let i=0;i<nums.length;++i){
-            sum+=nums[i];
-         }
-         console.log(`sum of no.s of array is : ${sum}`);
+function arrProb() {
+    console.log(`Length of array is : ${nums.length}`);
+    let maxNum = nums[0];
+    let randNum = Math.floor(Math.random() * nums.length);
+    console.log(randNum);
+    let mRepNum = nums[randNum];
+    let sum = 0, count = 0;
 
-         for(let i=0;i<nums.length;++i){
-            if(nums[i]>maxNum){
-                maxNum=nums[i];
-            }
-         }
-         console.log(`The largest no. in array is : ${maxNum}`);
-      
-         for(let i=0;i<nums.length;i++){
-            if(nums[i]==mRepNum){
-                mRepNum=nums[i];
-                count++;   
-            }
-         }
-         console.log(`Most repeated no. in array is ${mRepNum} and repeated ${count} times`);
+    for (let i = 0; i < nums.length; ++i) {
+        sum += nums[i];
+    }
+    console.log(`sum of no.s of array is : ${sum}`);
 
-       }  
-       arrProb();
+    for (let i = 0; i < nums.length; ++i) {
+        if (nums[i] > maxNum) {
+            maxNum = nums[i];
+        }
+    }
+    console.log(`The largest no. in array is : ${maxNum}`);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == mRepNum) {
+            mRepNum = nums[i];
+            count++;
+        }
+    }
+    console.log(`Most repeated no. in array is ${mRepNum} and repeated ${count} times`);
+
+}
+arrProb();
